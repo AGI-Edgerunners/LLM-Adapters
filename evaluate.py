c09d09faa7a76e734c27bf6b4623892d50111ae6
yihuai.lan <yihuai.lan@cengfine.com>
2023-04-03 02:16:38 +0800

    update test code

--- evaluate.py
+++ evaluate.py
@@ -163,7 +163,7 @@ def load_data(args) -> list:
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', choices=['AddSub', 'MultiArith', 'SingleEq'], required=True)
+    parser.add_argument('--dataset', choices=['AddSub', 'MultiArith', 'SingleEq', 'gsm8k','AQuA','SVAMP'], required=True)
     parser.add_argument('--model', choices=['LLaMA-7B', 'BLOOM-7B', 'GPT-j-6B'], required=True)
     parser.add_argument('--adapter', choices=['LoRA', 'AdapterP', 'AdapterH', 'Parallel', 'Scaled_Parallel'],
                         required=True)
