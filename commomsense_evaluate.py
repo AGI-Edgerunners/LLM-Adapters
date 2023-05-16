e0efdc0e8bb43875201e24528d3ee0fe2595876d
HZQ950419 <zhiqiang_hu@mymail.stud.edu.sg>
2023-05-16 23:02:09 +0700

    upload commonsense evaluate scripts

--- commomsense_evaluate.py
+++ commomsense_evaluate.py
@@ -35,17 +35,17 @@ def main(
     args = parse_args()
 
     def evaluate(
-            instruction,
+            instructions,
             input=None,
             temperature=0.1,
             top_p=0.75,
             top_k=40,
             num_beams=4,
-            max_new_tokens=256,
+            max_new_tokens=32,
             **kwargs,
     ):
-        prompt = generate_prompt(instruction, input)
-        inputs = tokenizer(prompt, return_tensors="pt")
+        prompts = [generate_prompt(instruction, input) for instruction in instructions]
+        inputs = tokenizer(prompts, return_tensors="pt", padding=True)
         input_ids = inputs["input_ids"].to(device)
         generation_config = GenerationConfig(
             temperature=temperature,
@@ -62,67 +62,48 @@ def main(
                 output_scores=True,
                 max_new_tokens=max_new_tokens,
             )
-        s = generation_output.sequences[0]
-        output = tokenizer.decode(s)
-        return output.split("### Response:")[1].strip()
+        s = generation_output.sequences
+        outputs = tokenizer.batch_decode(s, skip_special_tokens=True)
+        outputs = [o.split("### Response:")[1].strip() for o in outputs]
+        print(outputs)
+        return outputs
 
-    """
-    # testing code for readme
-    for instruction in [
-        "Tell me about alpacas.",
-        "Tell me about the president of Mexico in 2019.",
-        "Tell me about the king of France in 2019.",
-        "List all Canadian provinces in alphabetical order.",
-        "Write a Python program that prints the first 10 Fibonacci numbers.",
-        "Write a program that prints the numbers from 1 to 100. But for multiples of three print 'Fizz' instead of the number and for the multiples of five print 'Buzz'. For numbers which are multiples of both three and five print 'FizzBuzz'.",  # noqa: E501
-        "Tell me five words that rhyme with 'shock'.",
-        "Translate the sentence 'I have no mouth but I must scream' into Spanish.",
-        "Count up from 1 to 500.",
-    ]:
-        print("Instruction:", instruction)
-        print("Response:", evaluate(instruction))
-        print()
-    """
     save_file = f'experiment/{args.model}-{args.adapter}-{args.dataset}.json'
     create_dir('experiment/')
 
     dataset = load_data(args)
+    batches = create_batch(dataset, args.batch_size)
     tokenizer, model = load_model(args)
-    total = len(dataset)
+    total = len(batches)
     correct = 0
-    miss = 0.001
+    current = 0
     output_data = []
     pbar = tqdm(total=total)
-    for idx, data in enumerate(dataset):
-        instruction = data.get('instruction')
+    for idx, batch in enumerate(batches):
+        current += len(batch)
+        instructions = [data.get('instruction') for data in batch]
 
-        outputs = evaluate(instruction)
-        label = data.get('answer')
-        flag = False
-        if args.dataset.lower() in ['aqua']:
-            predict = extract_answer_letter(args, outputs)
+        outputs = evaluate(instructions)
+
+        for data, output in zip(batch, outputs):
+            label = data.get('answer')
+            flag = False
+            predict = extract_answer(args, output)
             if label == predict:
                 correct += 1
                 flag = True
-        else:
-            if isinstance(label, str):
-                label = float(label)
-            predict = extract_answer_number(args, outputs)
-            if abs(label - predict) <= miss:
-                correct += 1
-                flag = True
-        new_data = copy.deepcopy(data)
-        new_data['output_pred'] = outputs
-        new_data['pred'] = predict
-        new_data['flag'] = flag
-        output_data.append(new_data)
-        print(' ')
+            new_data = copy.deepcopy(data)
+            new_data['output_pred'] = output
+            new_data['pred'] = predict
+            new_data['flag'] = flag
+            output_data.append(new_data)
+            print(data["instruction"])
+            print(output)
+            print('prediction:', predict)
+            print('label:', label)
         print('---------------')
-        print(outputs)
-        print('prediction:', predict)
-        print('label:', label)
+        print(f'\rtest:{idx + 1}/{total} | accuracy {correct}  {correct / current}')
         print('---------------')
-        print(f'\rtest:{idx + 1}/{total} | accuracy {correct}  {correct / (idx + 1)}')
         with open(save_file, 'w+') as f:
             json.dump(output_data, f, indent=4)
         pbar.update(1)
@@ -174,16 +155,25 @@ def load_data(args) -> list:
     json_data = json.load(open(file_path, 'r'))
     return json_data
 
+def create_batch(dataset, batch_size):
+    batches = []
+    num_batch = len(dataset)//batch_size if len(dataset) % batch_size == 0 else len(dataset)//batch_size + 1
+    for i in range(num_batch):
+        batch = dataset[i*batch_size: min((i+1)*batch_size, len(dataset))]
+        batches.append(batch)
+    return batches
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', choices=['AddSub', 'MultiArith', 'SingleEq', 'gsm8k', 'AQuA', 'SVAMP'],
+    parser.add_argument('--dataset', choices=["boolq", "piqa", "social_i_qa", "hellaswag", "winogrande", "ARC-Challenge", "ARC-Easy", "openbookqa"],
                         required=True)
-    parser.add_argument('--model', choices=['LLaMA-7B', 'BLOOM-7B', 'GPT-j-6B'], required=True)
-    parser.add_argument('--adapter', choices=['LoRA', 'AdapterP', 'AdapterH', 'Parallel', 'Scaled_Parallel'],
+    parser.add_argument('--model', choices=['LLaMA-7B', "LLaMA-13B",'BLOOM-7B', 'GPT-j-6B'], required=True)
+    parser.add_argument('--adapter', choices=['LoRA', 'AdapterP', 'AdapterH', 'Parallel'],
                         required=True)
     parser.add_argument('--base_model', required=True)
     parser.add_argument('--lora_weights', required=True)
+    parser.add_argument('--batch_size', type=int, required=True)
     parser.add_argument('--load_8bit', action='store_true', default=False)
 
     return parser.parse_args()
@@ -206,10 +196,14 @@ def load_model(args) -> tuple:
         raise ValueError(f'can not find lora weight, the value is: {lora_weights}')
 
     load_8bit = args.load_8bit
-    if args.model == 'LLaMA-7B':
+    if "LLaMA" in args.model:
         tokenizer = LlamaTokenizer.from_pretrained(base_model)
     else:
         tokenizer = AutoTokenizer.from_pretrained(base_model)
+    tokenizer.padding_side = "left"
+    tokenizer.pad_token_id = (
+        0  # unk. we want this to be different from the eos token
+    )
     if device == "cuda":
         model = AutoModelForCausalLM.from_pretrained(
             base_model,
@@ -268,34 +262,39 @@ def load_instruction(args) -> str:
     return instruction
 
 
-def extract_answer_number(args, sentence: str) -> float:
-    dataset = args.dataset.lower()
-    if dataset in ["multiarith", "addsub", "singleeq", "gsm8k", "svamp"]:
-        sentence = sentence.replace(',', '')
-        pred = [s for s in re.findall(r'-?\d+\.?\d*', sentence)]
-        if not pred:
-            return float('inf')
-        pred_answer = float(pred[-1])
-    else:
-        raise NotImplementedError(' not support dataset: {}'.format(dataset))
-    if isinstance(pred_answer, str):
-        try:
-            pred_answer = float(pred_answer)
-        except ValueError as e:
-            pred_answer = float('inf')
-    return pred_answer
-
-
-def extract_answer_letter(args, sentence: str) -> str:
-    sentence_ = sentence.strip()
-    pred_answers = re.findall(r'A|B|C|D|E', sentence_)
-    if pred_answers:
+def extract_answer(args, sentence: str) -> float:
+    dataset = args.dataset
+    if dataset == 'boolq':
+        sentence_ = sentence.strip()
+        pred_answers = re.findall(r'true|false', sentence_)
         if not pred_answers:
-            return ''
+            return ""
         return pred_answers[0]
-    else:
-        return ''
+    elif dataset == 'piqa':
+        sentence_ = sentence.strip()
+        pred_answers = re.findall(r'solution1|solution2', sentence_)
+        if not pred_answers:
+            return ""
+        return pred_answers[0]
+    elif dataset in ['social_i_qa', 'ARC-Challenge', 'ARC-Easy', 'openbookqa']:
+        sentence_ = sentence.strip()
+        pred_answers = re.findall(r'answer1|answer2|answer3|answer4|answer5', sentence_)
+        if not pred_answers:
+            return ""
+        return pred_answers[0]
+    elif dataset == 'hellaswag':
+        sentence_ = sentence.strip()
+        pred_answers = re.findall(r'ending1|ending2|ending3|ending4', sentence_)
+        if not pred_answers:
+            return ""
+        return pred_answers[0]
+    elif dataset == 'winogrande':
+        sentence_ = sentence.strip()
+        pred_answers = re.findall(r'option1|option2', sentence_)
+        if not pred_answers:
+            return ""
+        return pred_answers[0]
 
 
 if __name__ == "__main__":
-    fire.Fire(main)
+    main()
